bbccc7983249612d495ea0509ad8a52432923b50
philiphester <69607375+philiphester@users.noreply.github.com>
2025-08-02 20:55:28 -0400

    Add Excercise 03 answer

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,25 @@
+// First answer, verbose and too much code. Actual solution below is much simpler, but perhaps not specific to the tests.
+// Solutions saves a return statement from my first solution.
+
+// function numberChecker(number) {
+//   if (number === 1000 || number === 10) {
+//     return true;
+//   } else if (number === 9 || number === 6) {
+//     return false;
+//   } else {
+//     return false;
+//   }
+// }
+
+// Odin solution
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
   }
 }
+
 
 // Do not edit below this line
 module.exports = numberChecker;
