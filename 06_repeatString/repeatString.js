9ef92efe629a13404c183c7b10b66498e303c4d3
philiphester <69607375+philiphester@users.noreply.github.com>
2025-09-12 17:10:31 -0400

    Add 06 exercise answer

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
-
+// My solution
+const repeatString = function(string, num) {
+    let newString = "";
+    if (num < 0) {
+        newString = "ERROR";
+    } else {
+        for (let i = 0; i < num; i++) {
+        newString = newString + string;
+        }
+    }
+    return newString;
 };
+
+// Revision based on Odin solution
+/*
+const repeatString2 = function(string, num) {
+    if(num < 0) return "ERROR";
+    let newString = "";
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    }
+    return newString;
+};
+*/
 
 // Do not edit below this line
 module.exports = repeatString;
