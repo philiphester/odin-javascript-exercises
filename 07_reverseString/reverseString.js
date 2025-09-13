c0ca98d1ed4ac4fb875e507999a9cbde735d0291
philiphester <69607375+philiphester@users.noreply.github.com>
2025-09-13 12:01:15 -0400

    Add exercrise 07 answer

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+// My solution
+const reverseString = function(string) {
+    let reversed = "";
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i];
+    }
+    return reversed;
 };
+
+// Coding the Odin solution - Using array methods
+/*
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
+};
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
