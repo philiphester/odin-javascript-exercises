da9f69b183cf6048b34a7c2866dc20b5dd825be5
philiphester <69607375+philiphester@users.noreply.github.com>
2025-09-13 20:34:51 -0400

    Add Exercise 10 answer

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,33 @@
-const leapYears = function() {
-
+// My solution
+const leapYears = function(year) {
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+
+// Odin Solution
+/*
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYeadDivisibleByFourHndred = year % 400 === 0;
+
+    if (isYearDivisibleByFour && (!isCentury || isYeadDivisibleByFourHndred)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+*/
 
 // Do not edit below this line
 module.exports = leapYears;
